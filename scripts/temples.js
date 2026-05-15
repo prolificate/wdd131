ab723ef559f6bdb93225a9b1d560d457d47ff8f1
Eniola <eniolafolabi105@gmail.com>
2026-05-15 15:53:41 +0100

    changrs

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,6 @@
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+
 const button = document.querySelector("#menu");
 const nav = document.querySelector("#nav");
 
--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,6 +1,7 @@
 const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
+
 const li = document.createElement("li");
 const deleteButton = document.createElement("button");
 
