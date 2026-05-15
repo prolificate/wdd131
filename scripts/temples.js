ebcda278a4a0318116b1e2ec8a42e5d6be42699e
Eniola <eniolafolabi105@gmail.com>
2026-05-15 15:05:45 +0100

    picture gallery

---
+++ scripts/temples.js
@@ -0,0 +1,7 @@
+const button = document.querySelector("#menu");
+const nav = document.querySelector("#nav");
+
+button.addEventListener("click", () => {
+    nav.classList.toggle('open');
+    button.classList.toggle('open');
+});
