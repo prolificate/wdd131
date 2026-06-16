ef36ce4461e5beadd9f4e534d8f3ccf1f9f24f48
Eniola <eniolafolabi105@gmail.com>
2026-06-16 09:45:43 +0100

    new javascript

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -1,46 +1,10 @@
-const site = [
-    {
-        imgURL: "https://www.bellanaija.com/wp-content/uploads/2025/10/SaveClip.App_574764437_18535598155039959_7801457239087537553_n.jpg",
-        name: "Culture",
-        link: "Discover Nigeria Culture"
-    },
-    {
-        imgURL: "https://www.cakesandcateringbyjoaida.com/wp-content/uploads/2026/01/c3288893-746c-47c2-89e0-d2765aeb86f7.jpg",
-        name: "Food",
-        link: "Explore Nigeria's Cuisines",
-    },
-    {
-        imgURL:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Zuma_Rock.jpg/330px-Zuma_Rock.jpg",
-        name: "Tourist Spot",
-        link: "Check out The Beauuty of Nideria"
-    },
-]
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
 
-const div = document.querySelector(".site");
+const button = document.querySelector("#menu");
+const nav = document.querySelector("nav");
 
-function display(list) {
-  div.innerHTML = "";
-
-  list.forEach((element) => {
-    const card = document.createElement("section");
-
-    const img = document.createElement("img");
-    img.src = element.url;
-    img.alt = element.name;
-    img.loading = "lazy";
-
-    const name = document.createElement("h3");
-    name.textContent = element.name;
-
-    const location = document.createElement("p");
-    location.textContent = element.location;
-
-    card.appendChild(img);
-    card.appendChild(name);
-    card.appendChild(location);
-
-    div.appendChild(card);
-  });
-}
-
-display(site);
+button.addEventListener("click", () => {
+    nav.classList.toggle('open');
+    button.classList.toggle('open');
+});
