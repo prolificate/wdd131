2987a13ccf2cd9633063efd67adcc150812af4ca
Eniola <eniolafolabi105@gmail.com>
2026-06-16 12:09:12 +0100

    assignment

---
+++ project/scripts/extra.js
@@ -0,0 +1,10 @@
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+
+const button = document.querySelector("#menu");
+const nav = document.querySelector("nav");
+
+button.addEventListener("click", () => {
+    nav.classList.toggle('open');
+    button.classList.toggle('open');
+});
